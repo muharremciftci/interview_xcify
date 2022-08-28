9b9dcd0f51b5c8864df9f26a95b80bc1152441d3
Muharrem Ciftci <muharremciftci34@gmail.com>
2022-08-28 17:37:41 +0300

    xcifytest

---
+++ xcifytest/xcifytest/Controllers/ProductController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using xcifytest.Models;
+
+namespace xcifytest.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+
+
+        private XcifySampleTestDB1Entities objProductDbEntities;
+
+        public ProductController()
+        {
+            objProductDbEntities = new XcifySampleTestDB1Entities();
+        }
+        public ActionResult Index()
+        {
+
+            
+            return View();
+        }
+
+        public JsonResult GetAllProduct()
+        {
+            var ProductGet = (from obj in objProductDbEntities.tblProducts select
+                              new
+                              {
+                                  Name = obj.Name,
+                                  Price= obj.Price
+                              }
+
+                            ).ToList();
+            return Json(data:new {Success=true,data=ProductGet }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ xcifytest/xcifytest/ViewModels/ProductViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace xcifytest.ViewModels
+{
+    public class ProductViewModel
+    {
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+
+
+    }
+}
